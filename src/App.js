bcc4d8f18790d8267e7e5f396b24e5b7ef298350
Jonathan <jdsw86@gmail.com>
2020-03-14 07:19:35 +1100

    day 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Amplify, { Auth } from 'aws-amplify';
 import awsconfig from './aws-exports';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Button } from '@material-ui/core'
 import Navbar from './components/navbar/Navbar'
+import { Hub } from 'aws-amplify';
 
 Amplify.configure(awsconfig);
 
 function App() {
+  const [user, setUser] = useState("")
+
+  useEffect(() => {
+
+    const listener = (data) => {
+      console.log(data)
+      if (data.payload.event === "signOut"){
+        setUser("");      
+      }
+      setUser(data.payload.data.username)
+    }
+    
+    Hub.listen('auth', listener);
+  })
+
   return (
     <React.Fragment>
     <CssBaseline />
       <div className="App">
+
       <Navbar/>
         <Button 
           variant="contained" 
@@ -20,7 +37,9 @@ function App() {
         >
             Open Hosted UI
         </Button>
+        <h1>Hello {user}</h1>
       </div>
+      
     </React.Fragment>
   );
 }
